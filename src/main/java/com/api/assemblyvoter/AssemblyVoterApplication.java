d47e75f5ff3004c4bbb382add95574c72a0ca8c5
Lucas Wilker <lwilker24@gmail.com>
2023-03-21 18:23:11 -0300

    Configuração Inicial SpringBoot Initializr

---
+++ src/main/java/com/api/assemblyvoter/AssemblyVoterApplication.java
@@ -0,0 +1,13 @@
+package com.api.assemblyvoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyVoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyVoterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/assemblyvoter/AssemblyVoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.assemblyvoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyVoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
