75c309ff846bc5125b688561263e402c756cde15
Lucas Wilker <lwilker24@gmail.com>
2023-03-22 02:28:28 -0300

    Agenda - Iniciando a criação da pauta com modelos de dto e db

--- src/main/java/com/api/assemblyvoter/models/AgendaModel.java
+++ src/main/java/com/api/assemblyvoter/models/AgendaModel.java
@@ -3,15 +3,16 @@ package com.api.assemblyvoter.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Set;
 
 @Entity
 @Table(name = "AgendaTable")
 @Data
-public class AgendaModel {
+public class AgendaModel implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
